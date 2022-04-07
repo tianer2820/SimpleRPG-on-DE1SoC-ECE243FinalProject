5f6a5f8ea1794751f3a9e7e6065b3f87b40151e1
Toby <toby.yang@mail.utoronto.ca>
2022-04-07 11:18:15 -0400

    add point and rect class

--- src/utils.h
+++ src/utils.h
@@ -7,5 +7,17 @@ void short2rgb(short color, unsigned char* r, unsigned char* g, unsigned char* b
 #define min(a,b) (((a)<(b))?(a):(b))
 #define max(a,b) (((a)>(b))?(a):(b))
 
+// simple data types
+typedef struct{
+    int x;
+    int y;
+} Point2D;
+
+typedef struct{
+    int x;
+    int y;
+    int w;
+    int h;
+} Rect2D;
 
 #endif // UTILS_H
